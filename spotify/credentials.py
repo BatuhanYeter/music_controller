d52d3c6d963fb1d195005d8b5d1a5c55b0eb15d6
Batuhan <45850467+BatuhanYeter@users.noreply.github.com>
2021-12-29 13:09:16 +0300

    Update credentials.py

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,4 +1,5 @@
-CLIENT_ID = "307822c73f274a21a982b528edcb3f32"
+# ID is hidden for now
+CLIENT_ID = "*"
 # I changed the secret key on spotify dev dashboard
 CLIENT_SECRET = "e2523f67ee1a43529b4e6e309e1ec81e"
 REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
